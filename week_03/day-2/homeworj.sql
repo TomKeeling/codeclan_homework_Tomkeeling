19017a9a91123d4358b25f41e883dfb09b2cfd92
TomKeeling <100126049+TomKeeling@users.noreply.github.com>
2022-03-23 09:01:04 +0000

    Create homeworj.sql

---
+++ week_03/day-2/homeworj.sql
@@ -0,0 +1,65 @@
+-- Question 1.
+---(a). Find the first name, last name and team name of employees who are members of teams. 
+
+
+SELECT 
+first_name,
+last_name,
+t.name
+FROM employees AS e
+INNER JOIN teams AS t 
+ON t.id = e.team_id
+
+
+--(b). Find the first name, last name and team name of employees 
+--who are members of teams and are enrolled in the pension scheme.
+
+SELECT 
+first_name,
+last_name,
+t.name
+FROM employees AS e
+INNER JOIN teams AS t 
+ON t.id = e.team_id
+WHERE pension_enrol = TRUE
+
+--(c). Find the first name, last name and team name of employees who ARE
+--members of teams, where their team has a charge cost greater than 80. 
+
+SELECT 
+first_name,
+last_name,
+name
+FROM employees AS e
+INNER JOIN teams AS t 
+ON t.id = e.team_id
+WHERE CAST(charge_cost AS int) > 80
+
+
+--(a). Get a table of all employees details, together with their local_account_no 
+-- and local_sort_code, if they have them.
+
+SELECT 
+first_name ,
+last_name,
+local_account_no,
+local_sort_code 
+FROM employees AS e
+INNER JOIN pay_details AS p 
+ON e.id = p.id
+
+
+--(b). Amend your query above to also return the name of the team that each employee belongs to. 
+SELECT 
+team,
+first_name ,
+last_name,
+local_account_no,
+local_sort_code 
+FROM (employees AS e
+INNER JOIN pay_details AS p 
+ON e.id = p.id)
+team
+
+
+
